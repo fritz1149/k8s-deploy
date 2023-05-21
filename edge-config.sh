3088f9ebf7cd2df74fcda15d9021b71d61a5b193
fritz1149 <1581225329@qq.com>
2023-05-21 12:52:33 +0000

    calico-helm

--- edge-config.sh
+++ edge-config.sh
@@ -9,7 +9,7 @@ docker pull eclipse-mosquitto:1.6.15
 docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy:v1.20.15
 docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy:v1.20.15 k8s.gcr.io/kube-proxy:v1.20.15
 docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy:v1.20.15
-token=9c29b980b1af2e5954dad028493b5b34c7cc68a60d1693fa1408b01db8e7b960.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODMwODAxNDZ9.Pr61z2HX1sOaEIa3mxqOqrcvbw0ZKWope_HuXI0q0jU
+token=a0aaf136b94e678805ac4a0e23fd1b0671844d1f4d06c41844e06535ee93f2eb.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODQ2Nzg1MTN9.pKRFnMl1UGyDDw1Ozy9W559o2sXUPGOrnc21ft90abY
 cloud=192.168.137.2
 chmod +x keadm
 ./keadm join --cloudcore-ipport=$cloud:10000 --token=$token 
--- k8s-master.sh
+++ k8s-master.sh
@@ -1,12 +1,12 @@
-kubeadm init \
+sudo kubeadm init \
     --pod-network-cidr=10.244.0.0/16 \
     --service-cidr=10.96.0.0/12 \
     --image-repository=registry.aliyuncs.com/google_containers
 
-mkdir -p $HOME/.kube
+sudo mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 sudo systemctl enable kubelet
 
-kubectl taint node vm-0 node-role.kubernetes.io/master-
-./keadm init --set cloudCore.modules.dynamicController.enable=true
+# kubectl taint node vm-0 node-role.kubernetes.io/master-
+# ./keadm init --set cloudCore.modules.dynamicController.enable=true --advertise-address=10.0.0.82
