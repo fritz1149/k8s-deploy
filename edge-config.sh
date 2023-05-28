02e697e75468db734d92f49d749cfcb8c7581851
fritz1149 <1581225329@qq.com>
2023-05-28 14:07:38 +0000

    1

--- edge-config.sh
+++ edge-config.sh
@@ -9,6 +9,7 @@ docker pull eclipse-mosquitto:1.6.15
 docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy:v1.20.15
 docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy:v1.20.15 k8s.gcr.io/kube-proxy:v1.20.15
 docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy:v1.20.15
+
 token=a0aaf136b94e678805ac4a0e23fd1b0671844d1f4d06c41844e06535ee93f2eb.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODQ2Nzg1MTN9.pKRFnMl1UGyDDw1Ozy9W559o2sXUPGOrnc21ft90abY
 cloud=192.168.137.2
 chmod +x keadm
